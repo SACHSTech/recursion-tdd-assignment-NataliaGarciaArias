39412ea62bd31ce1f1144a733cef1d68155ebcc9
NataliaGarciaArias <Natalia.Garciaarias21@ycdsbk12.ca>
2021-05-17 12:59:41 -0400

    organized imports on test file

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,11 +1,9 @@
 package recursionAssignment;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Before;
-import org.junit.jupiter.api.After;
 
 import recursionAssignment.Recursion;
-
 public class RecursionTest{
     
     @Before
