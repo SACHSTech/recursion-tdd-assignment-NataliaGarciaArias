d7b104f0a4327eeed1e3357f8a4b983d064aa2c8
NataliaGarciaArias <Natalia.Garciaarias21@ycdsbk12.ca>
2021-05-21 22:53:09 -0400

    cleaned up test file

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -6,21 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import recursionAssignment.Recursion;
 public class RecursionTest{
-    
-    @Before
-    public void beforeTest(){
-      // some steps to perform before tests are run
-    }
-    
-    /**
-     * An initial test
-     */
+  
+  //PROBLEM 1: COUNT 7
+
    @Test
     public void Problem1_Test1(){
-        // make assertion statement(s)
+
         assertEquals(2, Recursion.count7(717));
-        // assertEquals(EXPECTEDVALUE, TESTVALUE);
-        // assertTrue(TESTBOOLEAN);
     }
 
     @Test
@@ -43,6 +35,7 @@ public class RecursionTest{
       assertEquals(0, Recursion.count7(-1));
     }
 
+    //PROBLEM TWO: CHANGEPI
     @Test
     public void Problem2_Test1(){
       assertEquals("", Recursion.changePi(""));
@@ -62,6 +55,7 @@ public class RecursionTest{
       assertEquals("nothing", Recursion.changePi("nothing"));
     }
     
+    //PROBLEM THREE: STRING CLEAN
     @Test
     public void Problem3_Test1(){
       assertEquals("", Recursion.stringClean(""));
@@ -91,12 +85,5 @@ public class RecursionTest{
     public void Problem3_Test6(){
       assertEquals("i can't spel ", Recursion.stringClean("ii can'''t spppell   "));
     }
-    // add more tests
-    
-    @After
-    public void afterTest(){
-      // some steps to perform after tests are run
-    }
-    
     
 }
